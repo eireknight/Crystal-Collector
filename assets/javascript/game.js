a11c06646cb43b89c48a0482965cc3997cc6bd41
Patrick O'Brien <patricknathanobrien@gmail.com>
2020-03-02 19:48:06 -0500

    Pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,13 @@
   $( document ).ready(function(){
-  // Generates random number to guess
   var Random=Math.floor(Math.random()*102+19)
   
-  // Display random number
   $('#scoreToMatch').text(Random);
   
-  //Generate random number for each crystal
   var num1= Math.floor(Math.random()*12+1)
   var num2= Math.floor(Math.random()*12+1)
   var num3= Math.floor(Math.random()*12+1)
   var num4= Math.floor(Math.random()*12+1)
-  
-  // Variables to keep track of wins, losses and total
+
   var playerTotal= 0; 
   var wins= 0;
   var losses = 0;
@@ -20,7 +16,6 @@
 $('#wins').text(wins);
 $('#losses').text(losses);
 
-// Reset game
 function reset(){
       Random=Math.floor(Math.random()*102+19);
       console.log(Random)
@@ -32,26 +27,26 @@ function reset(){
       playerTotal= 0;
       $('#totalScore').text(playerTotal);
       } 
-// Display wins
+
 function woohoo(){
 alert("Congrats! You won!");
   wins++; 
   $('#wins').text(wins);
   reset();
 }
-// Display losses
+
 function loser(){
 alert ("Sorry! You lose!");
   losses++;
   $('#losses').text(losses);
   reset()
 }
-// Clicking crystals
+
   $('.red').on ('click', function(){
     playerTotal = playerTotal + num1;
     console.log("New playerTotal= " + playerTotal);
     $('#totalScore').text(playerTotal); 
-          //Win & lose conditions
+         
         if (playerTotal == Random){
           woohoo();
         }
